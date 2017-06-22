afd98f8fbf22738b4325632539ec22253f18f32e
ScarletMcLearn <ratul_shams@yahoo.com>
2017-06-22 08:30:25 +0600

    Add files via upload

---
+++ Front-End/Bootstrap/Get Bootstrap/Try 1/bootstrap/js/custom.js
@@ -0,0 +1,9 @@
+// $('#yourElement').addClass('animated bounceOutLeft');
+// $('#yourElement').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', doSomething);
+
+
+
+$('#my-head').addClass('animated pulse ');
+// $('#yourElement').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', doSomething);
+
+$('#bdy').addClass('animated fadeIn ');
