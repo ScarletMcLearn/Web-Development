f5ca4c6a00a63e4bf7c3637f066e6556aa1d37b4
ScarletMcLearn <ratul_shams@yahoo.com>
2018-07-09 16:21:35 +0600

    Add files via upload

--- Back-End/Django/Django By Example/4/bookmarks/bookmarks/urls.py
+++ Back-End/Django/Django By Example/4/bookmarks/bookmarks/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""bookmarks URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -16,17 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.contrib.sitemaps.views import sitemap
-from blog.sitemaps import PostSitemap
-
-sitemaps = {
-            'posts': PostSitemap,
-           }
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-         name='django.contrib.sitemaps.views.sitemap'),
-    
+    path('account/', include('account.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                         document_root=settings.MEDIA_ROOT)
--- Back-End/Django/Django By Example/4/mysite/blog/urls.py
+++ Back-End/Django/Django By Example/4/mysite/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+from .feeds import LatestPostsFeed
+
 app_name = 'blog'
 
 urlpatterns = [
@@ -10,5 +12,7 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
 
